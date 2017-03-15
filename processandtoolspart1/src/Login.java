e07db62b5c4656b29b80a3d741293f3c62d34510
Mitchell <s3603092@student.rmit.edu.au>
2017-03-15 11:37:56 +1100

    Update Login.java
    TO allow UserIO to work

--- processandtoolspart1/src/Login.java
+++ processandtoolspart1/src/Login.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 
 public class Login {
     public static void main(String[] args){
-
+        User[] users;
+        UserIO user = new UserIO();
         Scanner input = new Scanner(System.in);
         String username;
         String password;
+        try
+        {
+            users = user.initializeUsers();
+        } catch (FileNotFoundException e)
+        {
+            // TODO Auto-generated catch block
+            users = null;
+            e.printStackTrace();
+        }
+        for (int i = 0; i<users.length; i++){
+            System.out.println(users[i].getUsername());
+            System.out.println(users[i].getPassword());
 
-
-
-
+        }
 
         System.out.println("Welcome to the Online Booking System");
         System.out.println("------------------------------------");
@@ -20,14 +32,14 @@ public class Login {
         System.out.println("Please enter your Password");
         password = input.nextLine();
 
-        users check = new users(username, password);
+        //users check = new users(username, password);
 
-        if(check.auth()){
+        /*if(check.user.auth()){
             System.out.println("You are logged in");
         }
         else
             System.out.println("Invalid Input");
-
+    */
 
 
 
