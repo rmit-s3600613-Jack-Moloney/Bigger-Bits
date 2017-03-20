a07848fc61552c6d39d43e7ae6defb8f52e06cd8
rmit-s3606105-sam-holland <s3606105@student.rmit.edu.au>
2017-03-20 21:34:15 +1100

    Created registration process and other fix ups, inc Menu

--- src/CustomerRegister.java
+++ src/CustomerRegister.java
@@ -1,7 +1,90 @@
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CustomerRegister 
 {
+	public static Scanner scanner = new Scanner(System.in);
+	
 	public static void registration()
-	{
-		System.out.println("Registration Test");
+	{	
+		boolean loop = true;
+		
+		String username = null;
+		String password = null;
+		String checkPassword = null;
+
+		System.out.println("Enter your desired username: ");
+		
+		username = scanner.nextLine();
+		
+		try
+		{
+			File file = new File("users.txt");
+		
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) 
+			{
+				String lineFromFile = scanner.nextLine();
+				if(lineFromFile.contains(username)) 
+				{ 
+					System.out.println("Username " +username+ " is already taken, please select another");
+					registration();
+				}
+			}
+		}
+		
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		while(loop)
+		{	
+			System.out.println("Enter your desired password: ");
+			
+			password = scanner.nextLine();
+			
+			System.out.println("Confirm Password: ");
+			
+			checkPassword = scanner.nextLine();
+			
+			if(password.equals(checkPassword))
+			{
+				System.out.println("Passwords match!");
+				loop = false;
+			}
+			else
+			{
+				System.out.print("Passwords do not match, please try again.");
+				System.out.print("\n");
+			}
+		}
+		
+		String filename= "users.txt";
+		FileWriter fw;
+		
+		try 
+		{
+			fw = new FileWriter(filename,true);
+		   
+			fw.write(username);
+			fw.write(",");
+			fw.write(password);   
+		
+			fw.write('\n');
+			fw.close();
+			System.out.println("User registered!");
+			System.out.println("\n");
+		} 
+		
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		} 
+		
+		Menu.menu();
+
 	}
 }
