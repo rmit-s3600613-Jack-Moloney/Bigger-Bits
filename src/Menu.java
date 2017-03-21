9d2a9789860e1985ae31344e65288baca50d1b7f
rmit-s3603092-mitchell-sutton <s3603092@student.rmit.edu.au>
2017-03-21 14:19:20 +1100

    Update Login.java and Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -17,23 +17,19 @@ public class Menu {
 		
 		
 		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Welcome to Jack's Booking System");
-		System.out.println("--------------------------------");
-		System.out.println("Please select one of the following options:");
-		System.out.println("1.Login");
-		System.out.println("2.Customer Registration");
-		System.out.println("3.Quit");
-		System.out.println("--------------------------------");
-		System.out.println("Enter an option: ");
-		
-		
-		
 		boolean loop = true;
 		
 		//Loop for if the input is invalid or user wants to do another action
 		while(loop)
 		{
+			System.out.println("Welcome to Jack's Booking System");
+			System.out.println("--------------------------------");
+			System.out.println("Please select one of the following options:");
+			System.out.println("1.Login");
+			System.out.println("2.Customer Registration");
+			System.out.println("3.Quit");
+			System.out.println("--------------------------------");
+			System.out.println("Enter an option: ");
 			String option = input.nextLine();
 			
 			int optionNumber;
@@ -51,6 +47,12 @@ public class Menu {
 			{
 				case 1:
 					user = login.logInMenu(users);
+					if(user == null){
+						System.out.println("Log-in failed");
+					}
+					else{
+						System.out.println("THIS IS WHERE WE RUN THE BOOKING STUFF");
+					}
 					break;
 				case 2:
 					CustomerRegister.registration();
