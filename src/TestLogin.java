620773622086d5928c319ae1df6ae7c2011493aa
rmit-s3600613-Jack-Moloney <s3600613@student.rmit.edu.au>
2017-03-22 01:05:57 +1100

    Update tests

--- src/TestLogin.java
+++ src/TestLogin.java
@@ -14,6 +14,7 @@ public class TestLogin {
 	@After
 	public void afterTest(){
 		
+		
 	}
 	
 	@Test
--- src/TestRegistration.java
+++ src/TestRegistration.java
@@ -14,6 +14,7 @@ public class TestRegistration {
 	@After
 	public void afterTest(){
 		
+		
 	}
 	
 	@Test
