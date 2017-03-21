cb22e44fa51350bfb05f3ea0a084640c05be02c9
rmit-s3600613-Jack-Moloney <s3600613@student.rmit.edu.au>
2017-03-22 02:28:58 +1100

    More tests

--- src/TestLogin.java
+++ src/TestLogin.java
@@ -1,27 +1,33 @@
 import static org.junit.Assert.*;
 
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class TestLogin {
+	User[] users;
+	Login login = new Login();
 	
-	@Before
-	public void setup(){
-		
+	@Before public void setup(){
+		users[1] = new User("Jack","Moloney");
+		users[2] = new User("Mitchell","Sutton");
+		users[3] = new User("Samuel","Holland");
+		users[4] = new User("Dom","Alvaro");
+		login.users = users;
 	}
 	
 	@After
 	public void afterTest(){
-		
-		
+		login.users = null;	
 	}
 	
 	@Test
 	public void userNotInSystem() {
-		
-		searchUser("abc");
-		assertFalse();
+		boolean testVariable;
+		testVariable = login.searchUser("abc");
+		assertFalse(testVariable);
 	}
 	
 	@Test
