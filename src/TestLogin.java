8595379ff2ba8ed6c77d76e1b9235a14958787a5
rmit-s3600613-Jack-Moloney <s3600613@student.rmit.edu.au>
2017-03-22 01:01:32 +1100

    Update Tests

--- src/TestLogin.java
+++ src/TestLogin.java
@@ -1,9 +1,21 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestLogin {
-
+	
+	@Before
+	public void setup(){
+		
+	}
+	
+	@After
+	public void afterTest(){
+		
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
