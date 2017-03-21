a5c4c4698a0f5e4c3cb85064d0f57ade13fadcaf
rmit-s3603092-mitchell-sutton <s3603092@student.rmit.edu.au>
2017-03-22 10:59:30 +1100

    Update

---
+++ src/Owner.java
@@ -0,0 +1,28 @@
+
+public class Owner {
+
+    private String username;
+    private String password;
+    private String business;
+    private String name;
+    private String address;
+    private int phone;
+    
+    public Owner(String user, String pass, String bus, String name, String address, int num){
+        username = user;
+        password = pass;
+        business = bus;
+        this.name = name;
+        this.address = address;
+        phone = num;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}
