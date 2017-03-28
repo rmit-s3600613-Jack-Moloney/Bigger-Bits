a3308c5472621e349f1b05ef8891b29ade957484
rmit-s3603092-mitchell-sutton <s3603092@student.rmit.edu.au>
2017-03-28 21:02:42 +1100

    Update
    Documentation and logging

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.io.FileNotFoundException;
 public class Main 
 
   {	
+	/* Throws file not found exception as the class it goes to opens multiple files */
 	  public static void main(String[] args) throws FileNotFoundException{
 		  Menu menu = new Menu();
+		  /* Runs the menu function */
 		  menu.menu();
-
   }
 
 }
